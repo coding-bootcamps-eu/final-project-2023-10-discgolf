47ccd22117ee939bab53ab1f0ed99eec8abc89d9
Benjamin Lupa <147424978+BenjaminLupa@users.noreply.github.com>
2024-02-01 13:33:14 +0100

    feat: HoleMap.vue importiert

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/diskgolf2_1.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HoleMap />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import HoleMap from "@/components/HoleMap.vue";
 
 export default {
   name: "HomeView",
   components: {
     HelloWorld,
+    HoleMap,
   },
 };
 </script>
