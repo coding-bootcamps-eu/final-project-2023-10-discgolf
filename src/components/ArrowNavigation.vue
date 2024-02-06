4062138d2d0b1b6ee61dcb243348efe907188726
Daniel Schaffer <d.g.schaffer@gmail.com>
2024-02-06 12:56:30 +0100

    feat: added arrow navigation CSS

---
+++ src/components/ArrowNavigation.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="arrow-left">
+    <img src="../assets/arrow-left.png" alt="left" />
+  </div>
+  <div class="round-link">1</div>
+  <div class="round-link">2</div>
+  <div class="round-link">3</div>
+  <div class="arrow-right">
+    <img src="../assets/arrow-right.png" alt="right" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ArrowNavigation",
+};
+</script>
+
+<style scoped>
+.round-link {
+  background-color: #d5eae3;
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+  margin: 15px 6px;
+  font-size: 0.8rem;
+  font-weight: 600;
+  padding-top: 4px;
+}
+.arrow-left {
+  margin-top: 15px;
+  margin-right: 6px;
+  height: 25px;
+}
+.arrow-right {
+  margin-top: 15px;
+  margin-left: 6px;
+  height: 25px;
+}
+</style>
