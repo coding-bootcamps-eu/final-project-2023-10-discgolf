386afbab01a0016b09c06bc5420030605bc01559
Daniel Schaffer <d.g.schaffer@gmail.com>
2024-02-06 14:59:46 +0100

    footer component

---
+++ src/components/BalkenUnten.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="balken-bottom"></div>
+</template>
+
+<script>
+export default {
+  name: "BalkenUnten",
+};
+</script>
+
+<style scoped>
+.balken-bottom {
+  background-color: var(--dunkelgruen);
+  width: 393px;
+  height: 44px;
+}
+</style>
