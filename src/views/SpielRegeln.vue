aa89e9cb362c3fd22b1de4b6090169da58281e3d
Benjamin Lupa <147424978+BenjaminLupa@users.noreply.github.com>
2024-02-07 14:45:23 +0100

    weiter geändert

--- src/views/SpielRegeln.vue
+++ src/views/SpielRegeln.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="balken"></div>
   <div class="about"></div>
-  <router-link to="/nav">
+  <router-link class="router-link" to="/nav">
     <div><BackButton /></div>
   </router-link>
   <div class="menu">
